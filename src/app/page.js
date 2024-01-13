003b5f5f3a9887fe0bc4359575d4d1fd853a5b37
Abir <abirmahmud221@gmail.com>
2024-01-13 23:08:42 +0600

    first function

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-
+"use client"
 
 
 export default function Home() {
+
+  const fun = () => {
+    alert("hello world!")
+  }
+
   return (
     <main >
      <h1>Motin</h1>
+     <button onClick={fun}>Click Me</button>
     </main>
   )
 }

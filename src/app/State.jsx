fafb34a3ea390aa4a42bf480eaf983fb25e504ff
Abir <abirmahmud221@gmail.com>
2024-01-13 23:27:48 +0600

    first state declaration

--- src/app/State.jsx
+++ src/app/State.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
 
 const State = () => {
+
+    const [value, setValue] = useState("Abir Mahmud")
+
+    const add = () => {
+        setValue("Motin Mia")
+    }
+
+
     return (
         <div>
             <p>Hello {value}</p>
